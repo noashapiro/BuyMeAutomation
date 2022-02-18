4fafc2b3d8c28cf8ecea180647eab753886f5d43
noashapiro <noashapiro@gmail.com>
2022-02-18 11:24:31 +0200

    delete unused import

--- src/test/java/Pages/RegistrationPage.java
+++ src/test/java/Pages/RegistrationPage.java
@@ -2,7 +2,6 @@ package Pages;
 import Infrastructure.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class RegistrationPage extends BasePage {
 
